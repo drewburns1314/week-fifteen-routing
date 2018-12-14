2a8d1298411a1ddd0d6d2d598d69800327572680
drewburns1314 <37885864+drewburns1314@users.noreply.github.com>
2018-12-13 18:28:00 -0600

    added cat routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatComponent } from './cat/cat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cat', component: CatComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
